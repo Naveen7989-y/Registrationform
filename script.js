ea4b5bc3aa35d32627235b646e4ba31ee9612f00
Naveen7989-y <yerraguntanaveen7989@gmail.com>
2024-07-31 20:49:25 +0530

    First release of Hello World!

---
+++ script.js
@@ -0,0 +1,23 @@
+const firstname = document.querySelector('#firstname'); 
+const lastname = document.querySelector('#lasttname');
+const passkey = document.querySelector('#password');
+
+const generate = document.querySelector('#generate'); 
+
+const signup=document.querySelector('#signup');
+const createpassword= document.querySelector('#createpassword');
+const seebtn = document.querySelector('#seebtn'); 
+const email = document.querySelector('#email'); 
+const mobile= document.querySelector('#mobile'); 
+
+function seepassword(){
+    if(createpassword.type==='password'){
+        createpassword.type='text';
+        seebtn.innerText='Hide'
+    }
+    else{
+        createpassword.type='password';
+        seebtn.innerText='See';
+    }
+}
+seebtn.addEventListener('click',seepassword);
